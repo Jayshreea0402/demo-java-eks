27e778332462e3aa319f66f597a5071d2862d62f
jayshreea0402 <jayshreea0402@gmail.com>
2023-10-20 15:30:07 +0530

    adding deployment  files

--- check_changes.sh
+++ check_changes.sh
@@ -4,7 +4,7 @@
 CHANGED=false
 
 # List Terraform configuration file extensions
-TF_EXTENSIONS= (tf) #(tf tfvars)
+TF_EXTENSIONS=(tf) 
 
 # Loop through Terraform files and check for changes
 for EXTENSION in "${TF_EXTENSIONS[@]}"; do
