bc6155e5829ae791a8e069365750946f47f38ddb
jayshreea0402 <jayshreea0402@gmail.com>
2023-10-20 15:25:18 +0530

    adding deployment  files

--- check_changes.sh
+++ check_changes.sh
@@ -1,24 +1,14 @@
-# #!/bin/bash
-
-# if [[ $(git diff --quiet) -ne 0 ]]; then
-#     echo "Changes detected in Terraform templates."
-#     exit 0
-# else
-#     echo "No changes in Terraform templates. Skipping Terraform deployment."
-#     exit 1
-# fi
-
-!/bin/bash
+#!/bin/bash
 
 # Define a flag for change detection
 CHANGED=false
 
 # List Terraform configuration file extensions
-TF_EXTENSIONS= .tf #(tf tfvars)
+TF_EXTENSIONS= (tf) #(tf tfvars)
 
 # Loop through Terraform files and check for changes
 for EXTENSION in "${TF_EXTENSIONS[@]}"; do
-    if git diff --quiet -- "path/to/your/terraform/files/*.$EXTENSION"; then
+    if git diff --quiet -- "*.$EXTENSION"; then
         echo "No changes detected in *.$EXTENSION files."
     else
         echo "Changes detected in *.$EXTENSION files."
