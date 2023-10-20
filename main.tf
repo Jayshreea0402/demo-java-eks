64ce2649c96816810a5de91fd6503b8dffdddf4d
jayshreea0402 <jayshreea0402@gmail.com>
2023-10-20 14:46:13 +0530

    adding deployment  files

--- main.tf
+++ main.tf
@@ -16,13 +16,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = pmg-deploy #"education-eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
+  cluster_name = pmg-deploy 
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
