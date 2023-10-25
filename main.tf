3c779da700dead17bc5f7b8ee840f808733469ff
jayshreea0402 <jayshreea0402@gmail.com>
2023-10-25 13:59:30 +0530

    change in tf file

--- main.tf
+++ main.tf
@@ -4,9 +4,8 @@
 provider "aws" {
   region = var.region
 }
- 
 
-# Filter out local zones, which are not currently supported 
+# Filter out local zones, which are not currently supported
 # with managed node groups
 data "aws_availability_zones" "available" {
   filter {
@@ -16,8 +15,9 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = pmg-deploy 
+  cluster_name = "pmg-deploy" # Enclose it in double quotes as a string
 }
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.0.0"
@@ -58,7 +58,6 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
   eks_managed_node_groups = {
@@ -84,31 +83,13 @@ module "eks" {
   }
 }
 
-
-
-# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+provider "kubernetes-alpha" {
+  config_path = "~/.kube/config"
 }
 
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "4.7.0"
+# Example: Including a Kubernetes Deployment manifest
+resource "kubernetes_manifest" "webapp" {
+  provider = kubernetes-alpha
 
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
-
-resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.20.0-eksbuild.1"
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-  tags = {
-    "eks_addon" = "ebs-csi"
-    "terraform" = "true"
-  }
+  content = file("${path.module}/deployment.yaml")
 }
