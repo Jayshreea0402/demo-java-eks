eaacc1a0a1a015747eae478c992fd79da5f8db7e
jayshreea0402 <jayshreea0402@gmail.com>
2023-10-25 16:18:53 +0530

    creating only cluster

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "pmg-deploy-eks33" # Enclose it in double quotes as a string
+  cluster_name = "pmg-deploy-eks" # Enclose it in double quotes as a string
 }
 
 module "vpc" {
