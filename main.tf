be73cd2a25283cafed8a0cfbddb79596d16763ce
jayshreea0402 <jayshreea0402@gmail.com>
2023-10-30 14:34:12 +0530

    checking updates

--- main.tf
+++ main.tf
@@ -1,85 +1,74 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
+# Define the AWS provider
 provider "aws" {
-  region = var.region
+  region = "us-west-2"  # Set your desired AWS region
 }
 
-# Filter out local zones, which are not currently supported
-# with managed node groups
-data "aws_availability_zones" "available" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
-locals {
-  cluster_name = "java-terraform-pmg55" # Enclose it in double quotes as a string
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
-
-  name = "vpc-pmg"
-
-  cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
-
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+# Create a VPC
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/16"  # Specify your desired VPC CIDR range
+  enable_dns_support = true
   enable_dns_hostnames = true
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = 1
+  tags = {
+    Name = "eks-vpc"
   }
 }
 
+# Create subnets for the EKS cluster
+resource "aws_subnet" "eks_subnet" {
+  count = 3
+  vpc_id = aws_vpc.eks_vpc.id
+  cidr_block = element(["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"], count.index)
+  availability_zone = element(["us-west-2a", "us-west-2b", "us-west-2c"], count.index)
+  map_public_ip_on_launch = true
+}
+
+# Create an Internet Gateway for the VPC
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+# Attach the Internet Gateway to the VPC
+resource "aws_vpc_attachment" "eks_igw_attachment" {
+  vpc_id = aws_vpc.eks_vpc.id
+  internet_gateway_id = aws_internet_gateway.eks_igw.id
+}
+
+# Create a security group for the EKS cluster
+resource "aws_security_group" "eks_sg" {
+  name_prefix = "eks-"
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+# Allow inbound traffic to the EKS nodes (customize rules as needed)
+resource "aws_security_group_rule" "eks_sg_ingress" {
+  type        = "ingress"
+  from_port   = 0
+  to_port     = 65535
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.eks_sg.id
+}
+
+# Create an EKS cluster
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.3"
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.21"  # Specify your desired Kubernetes version
+  subnets         = aws_subnet.eks_subnet[*].id
+  vpc_id          = aws_vpc.eks_vpc.id
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.27"
-
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
-
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-  }
-
-  eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 2
-    }
-
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 4
-      desired_size = 1
+      instance_type = "t2.micro"  # Specify your desired instance type
+      key_name      = "my-key-pair"  # Specify your SSH key pair
     }
   }
 }
 
+# Output the EKS cluster endpoint
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
