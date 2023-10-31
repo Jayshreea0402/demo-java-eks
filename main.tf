1497ee259a878328352c92779cb0422e75007a24
jayshreea0402 <jayshreea0402@gmail.com>
2023-10-31 12:17:01 +0530

    switching to public subnet

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "java77-pmg88" # Enclose it in double quotes as a string
+  cluster_name = "pmg-cluster01" # Enclose it in double quotes as a string
 }
 
 module "vpc" {
