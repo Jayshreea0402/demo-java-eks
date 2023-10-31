4aac13b3dc1c3390bbde93bb9a7fb2a5fbf4cfbf
jayshreea0402 <jayshreea0402@gmail.com>
2023-10-31 11:04:13 +0530

    checking updates

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "java55-pmg55" # Enclose it in double quotes as a string
+  cluster_name = "java66-pmg66" # Enclose it in double quotes as a string
 }
 
 module "vpc" {
