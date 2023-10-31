02d190849ea758abe89c70b1666de033db186811
jayshreea0402 <jayshreea0402@gmail.com>
2023-10-31 10:50:22 +0530

    checking kms revomal

--- main.tf
+++ main.tf
@@ -58,8 +58,8 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
+    encrypt_boot = false
   }
-  map_roles = false 
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
