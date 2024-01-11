30389d652896d14b6f81f41452a3965032d9fe3b
jayshreea0402 <jayshreea0402@gmail.com>
2024-01-11 13:44:09 +0530

    adding all the files

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable dockerhub_credentials{
-    type = string
-    default = "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"
-}
-
-variable codestar_connector_credentials {
-    type = string
-    default = "arn:aws:codestar-connections:ap-south-1:779316103690:connection/aa14c1fb-d0f2-455c-8ad8-8b3930ee8039"
-}
